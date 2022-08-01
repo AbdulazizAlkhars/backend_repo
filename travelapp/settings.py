9da1bdf616e9b610cc550276531230c4fa6ce748
Ahmed Hajeyah <hajeyah@gmail.com>
2022-08-02 02:41:24 +0300

    Debug-off

--- travelapp/settings.py
+++ travelapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$!h9s#q=&iwoz2^&bb92!e75=e0&m4rvfiktfn4pmg%d$&q=v6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
