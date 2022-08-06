f2cd8a0f20b6fdffd14027b519e8e2f8c0a7f62e
Ahmed Hajeyah <hajeyah@gmail.com>
2022-08-06 23:01:36 +0300

    Add null is OK

--- trip/models.py
+++ trip/models.py
@@ -44,11 +44,11 @@ Wants to visit (boolean)
 class Trip(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     trip_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=100, default=True)
-    description = models.TextField(max_length=200, blank=True)
-    image = models.ImageField(upload_to='images', blank=True, default='/Users/ahmadcoded/Development/project/Travel_App/backend_repo/media/images/Simulator_Screen_Shot_-_iPhone_13_Pro_Max_-_2022-06-22_at_17.46.04.png')
-    country = models.CharField(max_length=100, default=True)
-    like = models.BooleanField(default=False)
-    wants_to_visit = models.BooleanField(default=False)
+    title = models.CharField(max_length=100, null=True, default="")
+    description = models.TextField(max_length=200, null=True, blank=True)
+    image = models.ImageField(upload_to='images',null=True ,blank=True, default='/Users/ahmadcoded/Development/project/Travel_App/backend_repo/media/images/Simulator_Screen_Shot_-_iPhone_13_Pro_Max_-_2022-06-22_at_17.46.04.png')
+    country = models.CharField(max_length=100, null=True, default="")
+    like = models.BooleanField(default=False, null=True)
+    wants_to_visit = models.BooleanField(default=False, null=True)
     def __str__(self):
         return self.title
